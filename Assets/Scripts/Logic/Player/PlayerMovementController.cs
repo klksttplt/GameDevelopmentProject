d3954a0a05f20d07c720b21d2c61c65e3c9bad2a
Ivan Pohodichev <ivan.pohodichev@gameloft.com>
2022-11-13 13:57:39 +0200

    Add graphics flip during movement

--- Assets/Scripts/Logic/Player/PlayerMovementController.cs
+++ Assets/Scripts/Logic/Player/PlayerMovementController.cs
@@ -1,9 +1,15 @@
 using Infrastructure.Services;
 using Logic.Common;
 using Services.Input;
+using UnityEngine;
 
 public class PlayerMovementController : ContextComponent
 {
+    // Fields: Editor
+
+    [SerializeField] 
+    private GameObject graphics;
+    
     // Services
 
     private IInputService inputService;
@@ -25,5 +31,21 @@ public class PlayerMovementController : ContextComponent
     private void FixedUpdate()
     {
         Moveable.Move(inputService.Axis);
+        if (inputService.Axis.x < 0)
+            Flip(false);
+        else if (inputService.Axis.x > 0)
+            Flip(true);
     }
+    
+    // Methods: Internal State
+
+    private void Flip(bool facingLeft)
+    {
+        var currentFacingLeft = graphics.transform.localScale.x > 0;
+        if(facingLeft && currentFacingLeft)
+            graphics.transform.localScale = new Vector3( graphics.transform.localScale.x * -1, graphics.transform.localScale.y, graphics.transform.localScale.z);
+        else if(!facingLeft && !currentFacingLeft)
+            graphics.transform.localScale = new Vector3(graphics.transform.localScale.x * -1, graphics.transform.localScale.y, graphics.transform.localScale.z);
+    }
+
 }
